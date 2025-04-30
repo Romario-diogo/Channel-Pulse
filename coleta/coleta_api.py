2584ad804e9b819ec6606cd087f76495840cf17c
2506roma <romariodiogo2021@gmail.com>
2025-04-30 07:08:27 -0300

    Primeira busca com API

--- coleta/coleta_api.py
+++ coleta/coleta_api.py
@@ -1,7 +1,64 @@
 
 from dotenv import load_dotenv
+import requests
 import os
+import json
 
 load_dotenv()
 key = os.getenv("KEYS_YOUTUBE")
-print(key)
+
+
+url = "https://www.googleapis.com/youtube/v3/search"
+
+params = {
+    "part": "snippet",
+    "q": "Felipe Neto",
+    "type": "channel",
+    "maxResults": 5,
+    "key": key
+}
+res = requests.get(url, params=params)
+dados = res.json()
+
+
+canais_ids = []
+for item in dados['items']:
+    canais_ids.append(item['id']['channelId'])
+
+
+canais_url = "https://www.googleapis.com/youtube/v3/channels"
+
+canis_params = {
+    "part": "snippet,statistics",
+    "id": ",".join(canais_ids),
+    "key": key
+}
+
+canis_res = requests.get(canais_url, params=canis_params)
+canais_data = canis_res.json()
+
+canal_oficial = None
+maior_numero = 0
+
+for canal in canais_data["items"]:
+    inscritos = int(canal["statistics"].get("subscriberCount", 0))
+    if inscritos > maior_numero:
+        maior_numero = inscritos
+        canal_oficial = canal
+
+if canal_oficial:
+    nome = canal_oficial["snippet"]["title"]
+    canal_id = canal_oficial["id"]
+    inscritos = canal_oficial["statistics"]["subscriberCount"]
+    descricao = canal_oficial["snippet"]["description"]
+
+    print("Canal mais relevantes encontrados:")
+    print(f"Nome: {nome}")
+    print(f"ID: {canal_id}")
+    print(f"Inscritos: {inscritos}")
+    print(f"Descrição: {descricao}")
+else:
+    print("Nada encontrado ")
+
+#with open("dados.json", 'w') as f:
+    #json.dump(dados, f , indent=2)
