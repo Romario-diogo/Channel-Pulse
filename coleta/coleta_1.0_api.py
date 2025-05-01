4a14f718f3e4a225e509dc153ec0ba53dba5038a
2506roma <romariodiogo2021@gmail.com>
2025-05-01 14:43:15 -0300

    def principal

--- coleta/coleta_1.0_api.py
+++ coleta/coleta_1.0_api.py
@@ -1,87 +1,76 @@
-# Busca canais no YouTube com base em uma palavra-chave.
-
-# Retorna os 5 primeiros canais relacionados.
-
-# Coleta informações detalhadas sobre cada um.
-
-# Compara o número de vídeos postados por cada canal.
-
-# Identifica qual canal tem mais vídeos publicados.
-
-#Exibe:
-
-#Nome do canal
-
-#ID do canal
-
-#Quantidade de vídeos (com separador de milhar)
-
-#Descrição
-
 from dotenv import load_dotenv
 import os
 import requests
-import json
+import json 
+load_dotenv()
 
-load_dotenv() # Carrga as variaveis 
 key = os.getenv("KEYS_YOUTUBE")
-
-channel = input("Qual o nome do canal ? ")
-
-url = "https://www.googleapis.com/youtube/v3/search"
-
-params = {
-    "part": "snippet",
-    "type": "channel",
-    "q": channel,
-    "maxResults": 4,
-    "key": key
-}
-
-res = requests.get(url, params=params)
-
-# Salvar em um json 
-dados = res.json()
-
-dados_info = []
-for info in dados["items"]:
-    dados_info.append(info["id"]["channelId"])
-
-url_channel = "https://www.googleapis.com/youtube/v3/channels"
-
-params = {
-    "part": "snippet, statistics",
-    "id": ','.join(dados_info),
-    "key": key
-}
-res_id = requests.get(url_channel, params=params)
-info = res_id.json()
+name_channel = str(input("Nome do canal !!!"))
 
 
-channel_info = None
-maior_video = 0
+def buscar_canal(name_channel, salvar_em_arquivo=False):
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+        "part":"snippet",
+        "type": "channel",
+        "q": name_channel,
+        "maxResults": 5,
+        "key": key
+    }
 
-for channel_video in info["items"]:
-    video = int(channel_video["statistics"].get("videoCount", 0))
-    if video > maior_video:
-        maior_video = video
-        channel_info = channel_video
+    resultado_params = requests.get(url, params=params) 
+    dados_params = resultado_params.json()
+
+    if not dados_params.get("items"):
+        print("Nenhum canal encontrado na busca.")
+        return
+
+    coleta_channel = []
+    for id_channel in dados_params['items']:
+        coleta_channel.append(id_channel['id']['channelId'])
+
+    url_channel = "https://www.googleapis.com/youtube/v3/channels"
+    params = {
+        "part": "snippet,statistics",
+        "id":",".join(coleta_channel),
+        "key": key
+    }
+    resultado_channel  = requests.get(url_channel, params=params)
+    dados_channel = resultado_channel.json()
+
+    if not dados_params.get("items"):
+        print("Nenhum canal encontrado na busca.")
+        return
+
+    channel_views = None
+    cont_views = 0
+    for channel in dados_channel['items']:
+        views = int(channel['statistics'].get("viewCount", 0))
+        if views > cont_views:
+            cont_views = views
+            channel_views = channel
+        if channel_views:
+           nome = channel_views["snippet"]['title']
+           id_channel = channel_views['id']
+           descricao = channel_views["snippet"]["description"]
+           print(f"Nome: {nome}")
+           print(f"ID: {id_channel}")
+           print(f"View: {cont_views:,}".replace(",", "."))
+           print(f"Descrição: {descricao}")
+        else:
+            print("Nenhum canal com visualizações foi identificado.")
+
+    if salvar_em_arquivo:
+        nome_arquivo = nome # Nome do canal
+        pasta_destino = "dados/brutos" # caminho do arquivo
+        os.makedirs(pasta_destino, exist_ok=True) # Define o diretório 
+        caminho_final = os.path.join(pasta_destino, nome_arquivo) # Junta caminho + nome do arquivo
+        with open (caminho_final, 'w', encoding="utf-8") as f:
+            json.dump(channel_views, f, indent=2)
+        print(f"Arquivo salvo em: {caminho_final}")
 
 
-if channel_info:
-    nome = channel_info["snippet"]["title"]
-    id = channel_info["id"]
-    video = int(channel_info["statistics"]["videoCount"])
-    descricao = channel_info["snippet"]["description"]
-#Exibe:
+buscar_canal(name_channel, salvar_em_arquivo=True)
 
-#Nome do canal
-print(f"Nome: {nome}")
-#ID do canal
-print(f"ID: {id}")
-#Quantidade de vídeos (com separador de milhar)
-print(f"Videos publicado: {video:,}".replace(",","."))
-#Descrição
-print(f"Descrição: {descricao}")
-# with open("res_id.json", 'w') as f:
-#     json.dump(info, f , indent=2)
+
+
